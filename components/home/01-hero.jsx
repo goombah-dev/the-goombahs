4bb784fdfb0391cf9e0c6cf80abd11b0a51e70e5
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2022-02-10 17:11:12 +0700

    Update hero text.

--- components/home/01-hero.jsx
+++ components/home/01-hero.jsx
@@ -26,17 +26,34 @@ export const Hero = () => {
           <LogoHero />
         </div>
         <p className={blurb}>
-          Weclome to The Goombahs, the NFT project that
+          An exclusive NFT-METAVERSE collection where each
+          token doubles as its holders IRL event and
+          metaverse entry pass. A Goombah is a unique 3D
+          evolving Goodfella-avatar that garners its rarity
+          based off rank within the organization. The Social
+          Club is the first of many build out and event
+          venues for not only The Goombahs but the world.
+          {/* Weclome to The Goombahs, the NFT project that
           works as a skeleton key in the metaverse. At the
           heart of it all enter Club Goombah the NFT social
           club where joining the family starts it all.
           Taking that sacred oath to the Omerta opens the
           doors and covers your back. Every Goodfella
           recieves access to the Club and what seems to be
-          an unlimited number of perks.
+          an unlimited number of perks. */}
           <br />
           <br />
-          After minting one of the 7500 unique 3d avatars
+          Community engagement, experience, and education
+          are the teams focal principals. Each being offered
+          and updated as our family and metaverse grows.
+          Minting is available, get your hands on a Goombah
+          and enables the first of many instant rewards. The
+          Infinite Goombah Giveaway Campaign. Now its time
+          to meet The Family.
+          <br />
+          <br />
+          Ah salute.
+          {/* After minting one of the 7500 unique 3d avatars
           you&apos;ll be a part of members only areas, as
           well as every and any builds from outside the
           family. No need for an invite or tickets to any
@@ -44,11 +61,14 @@ export const Hero = () => {
           well as entry to the “Infinite Goombah Giveaway”
           which is filled with exclusive merchandise ,
           Goombah airdrops(watch your gangster evolve), IRL
-          vacations, and any new Goombah related drops.
+          vacations, and any new Goombah related drops. */}
         </p>
         <div className={ctaButton}>
           <button className='btn btn__primary'>
-            <span>JOIN DISCORD</span> <DiscordIcon />
+            <a href='https://discord.com/invite/yXmRQy6s6Q'>
+              JOIN DISCORD
+            </a>{' '}
+            <DiscordIcon />
           </button>
           <button className='btn btn__secondary'>
             <Link href='/mint'>
