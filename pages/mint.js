041841b763b09d3c851bc04fca06d71f20db8a1a
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2022-02-11 15:57:54 +0700

    Remove commented out code.

--- pages/mint.js
+++ pages/mint.js
@@ -23,20 +23,6 @@ const mint = () => {
         Please note: Once you make the purchase, you cannot
         undo this action.
       </p>
-      {/* <h1 className={header}>Time to Join the Family</h1>
-      <p className={blurb}>
-        Current Phase: Whitelist Only, Next Phase: Public
-        Sale. Please do not try to mint unless your address
-        is qualified for the current phase. You can only
-        mint 1 Goombah at a time, but you can mint up to 5
-        total per whitelisted wallet. Please do not try and
-        mint more than 5. Make sure you are connected to the
-        right network (Ethereum Mainnet) and the correct address.
-        <br />
-        <br />
-        Please note: Once you make the purchase, you cannot
-        undo this action.
-      </p> */}
     </div>
   )
 };
