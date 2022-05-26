52601a50ea9791b39ee7e53e542534a3b95edbee
thelastonionring <90852641+thelastonionring@users.noreply.github.com>
2022-05-26 11:12:52 -0400

    Update 02-story.jsx

--- components/home/02-story.jsx
+++ components/home/02-story.jsx
@@ -23,15 +23,16 @@ export const Story = () => {
           <p>
            An exclusive NFT-METAVERSE collection where each token doubles as The Goombaverse Utility Pass. Instantly upon mint holders are forever granted entry into all IRL and metaverse events. Anything The Goombahs or their partners are apart of, you are now apart of.
            A Goombah is a unique 3D evolving Goodfella-avatar that garners its rarity based on rank within the organization. As our family continues to grow both in size and stature so will the Goombaverse. Our team is passionately dedicated on instilling our own “Omertà”. This carefully crafted ethos is centered around what will be consistently referred to as the three E's:         
-           <br />
-             Education, Experience, and Engagement.
-          <br />   
+          </p>
+          
+            <h3>Education, Experience, and Engagement</h3>
+         <p> 
           
             The Goombah Social Club provides a systematically superlative platform for execution. Revolving around the three E's the social club will initially offer a concert venue, horse track, Casino & MORE! This has all been built pre-launch and will continue to evolve. Possessing entry to The Families central headquarters warrants a constant engaging experience inside this new worlds top of the line virtual environment. Complete with tons of user engagement upgraded regularly! The social club is just the beginning.
+          </p>
+              <h3>One highlight of a utility that features Experience & engagement will be a recurring raffle!</h3>
           <br />
-              One highlight of a utility that features Experience & engagement will be a recurring raffle!
-          <br />
-            Now its time to meet The Family… ALLA SALUTE
+           <p> Now its time to meet The Family… ALLA SALUTE
        </p>
         </p>
       </div>
