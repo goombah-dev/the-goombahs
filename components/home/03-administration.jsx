090d25f4067357b9efa338bb9d705a8222b6a6fc
thelastonionring <90852641+thelastonionring@users.noreply.github.com>
2022-05-26 11:32:36 -0400

    Update 03-administration.jsx

--- components/home/03-administration.jsx
+++ components/home/03-administration.jsx
@@ -26,8 +26,8 @@ const sectionData = {
         <li>7. Entry into every recurring Prize Pool Raffle
       </ol>
       <span>
-      The Goombahs Prize Pool Raffle
-      Upon obtaining and verifying one or more Gen-1 Goombah NFT, the holder unlocks full access to The Goombahverse. Upon registration and verification of your wallet/NFT(s), each Goombah held will grant one entry into every recurring Prize Pool Raffle. A raffle will be held after approximately every 100 Goombah NFT mints for prizes up to $10,000 USD.
+      The Goombahs Prize Pool Raffle</span>
+      <span>Upon obtaining and verifying one or more Gen-1 Goombah NFT, the holder unlocks full access to The Goombahverse. Upon registration and verification of your wallet/NFT(s), each Goombah held will grant one entry into every recurring Prize Pool Raffle. A raffle will be held after approximately every 100 Goombah NFT mints for prizes up to $10,000 USD.
       </span>
         
     
